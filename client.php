1c689b00ecfb15978bc47dd3d19a245d8c274940
Sergei A <sa@e1media.ru>
2018-11-13 01:23:30 +0300

    client

--- client.php
+++ client.php
@@ -1,8 +1,5 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
-
 include 'file_transfer_lib.php';
 
 use FileTransfer as FT;
@@ -10,25 +7,45 @@ use FileTransfer as FT;
 $factory = new FT\Factory();
 
 try
+
 {
-    $conn = $factory->getConnection('ssh', 'user', 'pass', 'hostname.com', 2222);
-    $conn->cd('/var/www')
-    ->download('dump.tar.gz')
-    ->close();
+
+$conn = $factory->getConnection('ssh', 'user', 'pass', 'hostname.com', 2222);
+
+$conn->cd('/var/www')
+
+->download('dump.tar.gz')
+
+->close();
+
 }
+
 catch(Exception $e)
+
 {
-    echo $e->getMessage();
+
+echo $e->getMessage();
+
 }
 
 try
+
 {
-    $conn = $factory->getConnection('ftp', 'user', 'pass', 'hostname.com');
-    echo $conn->pwd() . "\n";
-    $conn->upload('archive.zip');
-    print_r($conn->exec('ls -al'));
+
+$conn = $factory->getConnection('ftp', 'user', 'pass', 'hostname.com');
+
+echo $conn->pwd() . "\n";
+
+$conn->upload('archive.zip');
+
+print_r($conn->exec('ls -al'));
+
 }
+
 catch (Exception $e)
+
 {
-    echo $e->getMessage();
-} 
+
+echo $e->getMessage();
+
+}
